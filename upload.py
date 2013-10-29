093d4e4c22701c32f905320567e324a3cb1c58b4
heren <zhanglihuahuahua@126.com>
2013-10-29 17:11:35 +0800

    back

--- upload.py
+++ upload.py
@@ -4,14 +4,19 @@ import cherrypy
 import cgi
 import tempfile
 import memcache
+import uuid
+import os
 
 mc = memcache.Client(['127.0.0.1:11211'])
-
+imgroot = "img"
 class myFieldStorage(cgi.FieldStorage):
-
+        
     def make_file(self, binary=None):
-
-        return open('workfile.png', 'w+b')
+        paths_list = self.headers['stored_file_path'].split("/")
+        dirname = "/".join([imgroot,paths_list[0],paths_list[1],paths_list[2]]);
+        if not os.path.exists(dirname):
+            os.makedirs(dirname);
+        return open(imgroot+"/"+self.headers['stored_file_path'], 'w+b')
 
 
 def noBodyProcess():
@@ -44,7 +49,10 @@ class fileUpload:
         lcHDRS = {}
         for key, val in cherrypy.request.headers.iteritems():
             lcHDRS[key.lower()] = val
-
+            
+        uid = uuid.uuid4();
+        stored_path = '/'.join([uid.hex[0:3], uid.hex[4:6], uid.hex[7:9],uid.hex[10:]]);
+        lcHDRS["stored_file_path"] = stored_path
         formFields = myFieldStorage(fp=cherrypy.request.rfile,
                                     headers=lcHDRS,
                                     environ={'REQUEST_METHOD':'POST'},
@@ -53,7 +61,7 @@ class fileUpload:
         theFile = formFields['theFile']
         #os.link(theFile.file.name, theFile.filename)
 
-        return "ok, got it filename='%s'" % theFile.filename
+        return "%s" % stored_path
 
     @cherrypy.expose
     def getImg(self,fileName=None):
@@ -63,10 +71,10 @@ class fileUpload:
         #print mc.get("hello")
         content = mc.get(fileName.encode("utf-8"))
         if  content == None:
-            imgFile = open(fileName,"rb")
+            imgFile = open(imgroot+"/"+fileName,"rb")
             content = imgFile.read()
             mc.set(fileName.encode("utf-8"),content)
-        cherrypy.response.headers['Content-Type'] = "image/png"
+        cherrypy.response.headers['Content-Type'] = "image"
         return content
 
 
