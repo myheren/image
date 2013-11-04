50be7fecde3d77d58bcc78687f8e884ae08d4c7a
heren <zhanglihuahuahua@126.com>
2013-11-04 14:07:39 +0800

    interface

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from cherrypy import wsgiserver
 from upload import application
-server = wsgiserver.CherryPyWSGIServer(('localhost', 8080), application)
+server = wsgiserver.CherryPyWSGIServer(('localhost', 8000), application)
 try:
     server.start()
 except KeyboardInterrupt:
